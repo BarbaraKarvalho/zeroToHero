8552d9296a2503c9118d7651e2172aacd8bf6783
BarbaraKarvalho <barbara_silvac@hotmail.com>
2023-12-07 14:15:08 -0300

    Adjusted the variable name

--- src/java/exercises/src/main/java/com/zeroToHero/exercise04.java
+++ src/java/exercises/src/main/java/com/zeroToHero/exercise04.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class exercise04 {
 
     public static void main(String[] args) {
-    float metros, centrimentros;
+    float metros, centrimetros;
 
     Scanner meters = new Scanner(System.in);
 
     System.out.println("Input the meters: ");
     metros = meters.nextFloat();
 
-    centrimentros = metros * 100;
-    //System.out.println("The value in centimeters is " + centrimentros);
-    System.out.printf("The value in centimeters is %.2f\n ", centrimentros);
+    centrimetros = metros * 100;
+    //System.out.println("The value in centimeters is " + centrimetros);
+    System.out.printf("The value in centimeters is %.2f\n ", centrimetros);
     meters.close();
     }
 }
