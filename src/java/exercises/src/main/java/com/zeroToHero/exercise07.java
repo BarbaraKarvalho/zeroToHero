24bdd8492fa5857ab4b99669ef993f9355bc2df5
BarbaraKarvalho <barbara_silvac@hotmail.com>
2023-12-09 14:54:09 -0300

    Calculation of fish weight

--- src/java/exercises/src/main/java/com/zeroToHero/exercise07.java
+++ src/java/exercises/src/main/java/com/zeroToHero/exercise07.java
@@ -1,28 +1,42 @@
 package com.zeroToHero;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class exercise07 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
         float pesoPeixe, excessoPeixe, multa;
+        boolean controleFluxo = true;
+        int menu;
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("João, how many kilos are in the fish? ");
-        pesoPeixe = scan.nextFloat();
+        while (controleFluxo) {
+            System.out.println("João, how many kilos are in the fish? ");
+            pesoPeixe = scan.nextFloat();
 
-        if(pesoPeixe <= 50) {
-            System.out.println("Your fish is complies with the weight established by the fishing regulations of the state of São Paulo");
+            if (pesoPeixe <= 50) {
+                System.out.println("Your fish is complies with the weight established by the fishing regulations of the state of São Paulo");
 
-        } else if (pesoPeixe >= 51) {
-            excessoPeixe = pesoPeixe - 50;
-            multa = excessoPeixe * 4;
+            } else {
+                excessoPeixe = pesoPeixe - 50;
+                multa = excessoPeixe * 4;
 
-            System.out.printf("Excess fish: %.2f\n", excessoPeixe);
-            System.out.printf("Fine for excess fish: %.2f\n", multa);
+                System.out.printf("Excess fish: %.2f\n", excessoPeixe);
+                System.out.printf("Fine for excess fish: %.2f\n", multa);
 
-            System.out.println("Your fish is above the weight established by the fishing regulations of the state of São Paulo");
-            scan.close();
+                System.out.println("Your fish is above the weight established by the fishing regulations of the state of São Paulo");
+            }
+
+            System.out.println("Do you wish to information other fish?");
+            System.out.println("1 - yes");
+            System.out.println("2 - no");
+            menu = scan.nextInt();
+
+            if (menu == 2) {
+                controleFluxo = false;
+            }
         }
+        scan.close();
     }
 }
