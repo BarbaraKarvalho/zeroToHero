edb0ba51b303321c4eafffc63d07cbd650ee7d07
Rita Calixto <ritaksacalixto@gmail.com>
2023-12-13 18:22:46 -0300

    Desafio 1 Concluido com sucesso

--- src/python/src/zero1.py
+++ src/python/src/zero1.py
@@ -1,1 +1,7 @@
-#teste
+
+raio = float(input("Digite o raio :"))
+area = round((3.14 *raio),2)
+perimetro = round(2 * 3.14 * raio)
+
+print ('Aréa igual a :' ,area)
+print ('Perimetro igual a :', perimetro)
