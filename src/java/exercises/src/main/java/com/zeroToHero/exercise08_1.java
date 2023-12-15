d396a67bd884a7e1bb45465485a50ff66c0d1986
BarbaraKarvalho <barbara_silvac@hotmail.com>
2023-12-15 20:28:46 -0300

    Calculation of the IR according to salary

--- src/java/exercises/src/main/java/com/zeroToHero/exercise08_1.java
+++ src/java/exercises/src/main/java/com/zeroToHero/exercise08_1.java
@@ -7,59 +7,108 @@ public class exercise08_1 {
     private static final float SINDICATO = 0.05f;
 
     public static void main(String [] args) {
-        float valorHora, salarioBruto, salarioLiquido, descontoSalario;
+        float valorHora, salarioBruto, salarioLiquido, descontoSalario, descontoIR, descontoINSS, descontoSindicato;
         int quantHora;
+        boolean controleFluxo = true;
+        int menu;
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("How much do you earn per hours worked? ");
-        valorHora = scan.nextFloat();
+        while (controleFluxo) {
 
-        System.out.println("How many hours do you work? ");
-        quantHora = scan.nextInt();
+            System.out.println("How much do you earn per hours worked? ");
+            valorHora = scan.nextFloat();
 
-        salarioBruto = valorHora * quantHora;
-        System.out.printf("Your gross salary is R$ %.2f\n", salarioBruto);
+            System.out.println("How many hours do you work? ");
+            quantHora = scan.nextInt();
 
-        if(salarioBruto < 2112.00){
-            descontoSalario = salarioBruto * (INSS + SINDICATO);
-            System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+            salarioBruto = valorHora * quantHora;
+            System.out.printf("Your gross salary is R$ %.2f\n\n", salarioBruto);
 
-            salarioLiquido = salarioBruto - descontoSalario;
-            System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+            if (salarioBruto < 2112.00) {
+                final float IR = 0;
+                descontoIR = salarioBruto * IR;
+                System.out.printf("IR (zero): R$ %.2f\n", descontoIR);
+                descontoINSS = salarioBruto * INSS;
+                System.out.printf("INSS (8): R$ %.2f\n", descontoINSS);
+                descontoSindicato = salarioBruto * SINDICATO;
+                System.out.printf("SINDICATO (5): R$ %.2f\n", descontoSindicato);
 
-        } else if (salarioBruto >= 2112.00 && salarioBruto < 2826.65){
-            final float IR = 0.075f;
-            descontoSalario = salarioBruto * (0.075f + INSS + SINDICATO);
-            System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+                descontoSalario = salarioBruto * (INSS + SINDICATO);
+                System.out.printf("Total discount is R$ %.2f\n\n", descontoSalario);
 
-            salarioLiquido = salarioBruto - descontoSalario;
-            System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+                salarioLiquido = salarioBruto - descontoSalario;
+                System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
 
-        } else if (salarioBruto >= 2826.66 && salarioBruto < 3751.05){
-            final float IR = 0.15f;
-            descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
-            System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+            } else if (salarioBruto >= 2112.00 && salarioBruto < 2826.65) {
+                final float IR = 0.075f;
+                descontoIR = salarioBruto * IR;
+                System.out.printf("IR (7.5): R$ %.2f\n", descontoIR);
+                descontoINSS = salarioBruto * INSS;
+                System.out.printf("INSS (8): R$ %.2f\n", descontoINSS);
+                descontoSindicato = salarioBruto * SINDICATO;
+                System.out.printf("SINDICATO (5): R$ %.2f\n", descontoSindicato);
 
-            salarioLiquido = salarioBruto - descontoSalario;
-            System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+                descontoSalario = salarioBruto * (0.075f + INSS + SINDICATO);
+                System.out.printf("Total discount is R$ %.2f\n\n", descontoSalario);
 
-        } else if (salarioBruto >= 3751.06 && salarioBruto < 4664.68){
-            final float IR = 0.225f;
-            descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
-            System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+                salarioLiquido = salarioBruto - descontoSalario;
+                System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
 
-            salarioLiquido = salarioBruto - descontoSalario;
-            System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+            } else if (salarioBruto >= 2826.66 && salarioBruto < 3751.05) {
+                final float IR = 0.15f;
+                descontoIR = salarioBruto * IR;
+                System.out.printf("IR (15): R$ %.2f\n", descontoIR);
+                descontoINSS = salarioBruto * INSS;
+                System.out.printf("INSS (8): R$ %.2f\n", descontoINSS);
+                descontoSindicato = salarioBruto * SINDICATO;
+                System.out.printf("SINDICATO (5): R$ %.2f\n", descontoSindicato);
 
-        } else if (salarioBruto > 4664.68){
-            final float IR = 0.275f;
-            descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
-            System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+                descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
+                System.out.printf("Total discount is R$ %.2f\n\n", descontoSalario);
 
-            salarioLiquido = salarioBruto - descontoSalario;
-            System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
-            scan.close();
+                salarioLiquido = salarioBruto - descontoSalario;
+                System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+
+            } else if (salarioBruto >= 3751.06 && salarioBruto < 4664.68) {
+                final float IR = 0.225f;
+                descontoIR = salarioBruto * IR;
+                System.out.printf("IR (22.5): R$ %.2f\n", descontoIR);
+                descontoINSS = salarioBruto * INSS;
+                System.out.printf("INSS (8): R$ %.2f\n", descontoINSS);
+                descontoSindicato = salarioBruto * SINDICATO;
+                System.out.printf("SINDICATO (5): R$ %.2f\n", descontoSindicato);
+
+                descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
+                System.out.printf("Your discount is R$ %.2f\n", descontoSalario);
+
+                salarioLiquido = salarioBruto - descontoSalario;
+                System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+
+            } else if (salarioBruto > 4664.68) {
+                final float IR = 0.275f;
+                descontoIR = salarioBruto * IR;
+                System.out.printf("IR (27.5): R$ %.2f\n", descontoIR);
+                descontoINSS = salarioBruto * INSS;
+                System.out.printf("INSS (8): R$ %.2f\n", descontoINSS);
+                descontoSindicato = salarioBruto * SINDICATO;
+                System.out.printf("SINDICATO (5): R$ %.2f\n", descontoSindicato);
+
+                descontoSalario = salarioBruto * (IR + INSS + SINDICATO);
+                System.out.printf("Total discount is R$ %.2f\n\n", descontoSalario);
+
+                salarioLiquido = salarioBruto - descontoSalario;
+                System.out.printf("Your total salary for month is R$ %.2f\n", salarioLiquido);
+            }
+            System.out.println("Do you want other information?");
+            System.out.println("1 - yes");
+            System.out.println("2 - no");
+            menu = scan.nextInt();
+
+            if (menu == 2) {
+                controleFluxo = false;
+            }
         }
+        scan.close();
     }
 }
