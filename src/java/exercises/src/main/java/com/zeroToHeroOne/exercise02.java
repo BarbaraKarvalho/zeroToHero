97cd39c9646dbdde03b5df0e2c9cc0b9ce7aa524
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-01-17 13:31:09 -0300

    New exercise(2)

---
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise02.java
@@ -0,0 +1,36 @@
+package com.zeroToHeroOne;
+
+import java.util.Scanner;
+
+public class exercise02 {
+    public static void main(String[] args) {
+
+        double numero;
+        boolean novaConsulta = true;
+        int menu;
+        Scanner scan = new Scanner(System.in);
+
+        while(novaConsulta) {
+            System.out.println("Insira um número: ");
+            numero = scan.nextDouble();
+
+            if (numero % 2 == 0 && numero > 0) {
+                System.out.println("O seu número é par e positivo");
+            } else if (numero % 2 == 0 && numero < 0) {
+                System.out.println("O seu número é par e negativo");
+            } else if (numero % 2 == 1 && numero > 0) {
+                System.out.println("O seu número é impar e positivo");
+            } else
+                System.out.println("O seu número é impar e negativo");
+
+            System.out.println("1 - Sim");
+            System.out.println("2 - Não");
+            menu = scan.nextInt();
+
+            if(menu == 2){
+                novaConsulta = false;
+                scan.close();
+            }
+        }
+    }
+}
