66d25f5be22b465f28a8eafbce8cd836db4ef28f
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-01-22 19:52:21 -0300

    New exercise (18) and (19)

--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise18.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise18.java
@@ -1,4 +1,32 @@
 package com.zeroToHeroOne;
 
+import java.util.Scanner;
+
 public class exercise18 {
+   // private static final double Francisco = 1.50f;
+  //  private static final double Sara = 1.10f;
+
+    public static void main(String[] args){
+
+        int ano = 0;
+        float Francisco = 1.50f;
+        float Sara = 1.10f;
+
+        while(Francisco >= Sara) {
+
+            Francisco += 0.02f;
+            System.out.printf("Por ano, Franscisco cresce: %.2f\n", Francisco);
+
+            Sara += 0.03f;
+            System.out.printf("Por ano, Sara cresce: %.2f\n", Sara);
+            System.out.println("");
+
+            ano++;
+
+            System.out.printf("Francisco: %.2f\t Sara: %.2f\t Ano: %d\n ", Francisco, Sara, ano);
+            System.out.println("");
+            //%t formats date/time
+        }
+        System.out.printf("São necessários %d anos! ", ano);
+    }
 }
--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise19.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise19.java
@@ -1,4 +1,18 @@
 package com.zeroToHeroOne;
 
 public class exercise19 {
+    public static void main(String[] args) {
+
+            System.out.println("TABUADA DE 0 A 10");
+
+        for(int tabuada = 0; tabuada <= 10; tabuada++) {
+
+            for(int contador = 0; contador <=10; contador++) {
+                int multiplicador = tabuada * contador;
+                System.out.println(tabuada + " x " + contador + " = " + multiplicador);
+            }
+            System.out.println(" "); //pular linnha
+        }
+    }
 }
+
