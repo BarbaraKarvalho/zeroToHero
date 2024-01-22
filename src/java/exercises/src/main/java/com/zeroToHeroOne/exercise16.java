0d9abbefe9e158df6df737283c767723c139d03d
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-01-22 10:13:05 -0300

    New exercise (16) and (17)

--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise16.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise16.java
@@ -1,4 +1,47 @@
 package com.zeroToHeroOne;
 
+import java.util.Scanner;
+
 public class exercise16 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        double x, y, z;
+        int menu;
+        boolean novaConsulta = true;
+
+        while(novaConsulta) {
+            System.out.println("Digite o primeiro valor válido");
+            x = scan.nextInt();
+            System.out.println("Digite o segundo valor válido");
+            y = scan.nextInt();
+            System.out.println("Digite o terceiro valor válido");
+            z = scan.nextInt();
+
+            if (x < y + z || y < x + z || z < y + x) {
+                System.out.println("É um Triângulo!");
+
+                if (x == y && x == z) {
+                    System.out.println("Triângulo Equilatero, ou seja, os três lados são iguais");
+
+                } else if (x == y || x == z) {
+                    System.out.println("Triângulo Isosceles, ou seja, os dois lados são iguais");
+
+                } else
+                    System.out.println("Triângulo Escaleno, ou seja, os três lados são diferentes");
+
+                System.out.println("Deseja verifcar novamente?");
+                System.out.println("1 - Sim");
+                System.out.println("2 - Não");
+                menu = scan.nextInt();
+
+                if (menu == 2) {
+                    novaConsulta = false;
+                }
+            }
+        }
+        scan.close();
+    }
 }
--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise17.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise17.java
@@ -1,4 +1,32 @@
 package com.zeroToHeroOne;
 
+import java.util.Scanner;
+
 public class exercise17 {
+    public static void main(String[] args) {
+        double Fahrenheit;
+        double Celsius;
+        int menu;
+        boolean novaConversao = true;
+
+        Scanner scan = new Scanner(System.in);
+        while (novaConversao) {
+            System.out.println("Digite a temperatura em Fahrenheit:");
+            Fahrenheit = scan.nextDouble();
+
+            Celsius = (5 * (Fahrenheit - 32) / 9);
+            System.out.printf("A temperatura em Fahrenheit é: %.2f\n", Fahrenheit);
+            System.out.printf("A temperatura em Celsius é: %.2f\n", Celsius);
+
+            System.out.println("Deseja fazer uma nova conversão de Fahrenheit para Celsius?");
+            System.out.println("1 - Sim");
+            System.out.println("2 - Não");
+            menu = scan.nextInt();
+
+            if (menu == 2) {
+                novaConversao = false;
+            }
+        }
+        scan.close();
+    }
 }
