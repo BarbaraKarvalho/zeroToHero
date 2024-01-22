cb20fa2d25464df376c55c969357ceb63955fb7a
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-01-22 20:29:02 -0300

    New exercise (20) and (21)

--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise20.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise20.java
@@ -1,4 +1,35 @@
 package com.zeroToHeroOne;
 
+import java.util.Scanner;
+
 public class exercise20 {
+    public static void main(String[] args){
+
+        int x, multiplicador;
+        int contador = 0;
+        int menu;
+        boolean novaTabuada = true;
+
+        Scanner scan = new Scanner(System.in);
+        while(novaTabuada) {
+            System.out.println("*** RESOLVEDOR DE TABUADA *** ");
+            System.out.println("Digite um número inteiro:");
+            x = scan.nextInt();
+
+            for(contador = 0; contador <= 10; contador++) {
+                multiplicador = x * contador;
+                System.out.println(x + " x " + contador + " = " + multiplicador);
+            }
+                System.out.println();
+                System.out.println("Deseja verifcar uma nova tabuada?");
+                System.out.println("1 - Sim");
+                System.out.println("2 - Não");
+                menu = scan.nextInt();
+
+                if (menu == 2) {
+                    novaTabuada = false;
+            }
+        }
+        scan.close();
+    }
 }
--- src/java/exercises/src/main/java/com/zeroToHeroOne/exercise21.java
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise21.java
@@ -1,4 +1,34 @@
 package com.zeroToHeroOne;
 
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
 public class exercise21 {
+    public static void main(String[] args) {
+
+        boolean novaRodada = true;
+        int menu;
+
+        Scanner scan = new Scanner(System.in);
+
+        while(novaRodada) {
+            Random gerador = new Random();
+            System.out.println("Os números aleatórios da rodada são: ");
+            for (int i = 0; i <= 100; i++) {
+                System.out.println(gerador.nextInt(0, 101));
+            }
+
+            System.out.println();
+            System.out.println("Deseja fazer uma nova consulta de números aleatórios?");
+            System.out.println("1 - Sim");
+            System.out.println("2 - Não");
+            menu = scan.nextInt();
+
+            if (menu == 2) {
+                novaRodada = false;
+            }
+        }
+        scan.close();
+    }
 }
