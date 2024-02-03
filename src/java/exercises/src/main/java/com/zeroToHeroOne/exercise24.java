ba63e2ee06edebb9fd9c7f0b9613acc47940f1a1
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-02-03 17:24:44 -0300

    New exercise 24

---
+++ src/java/exercises/src/main/java/com/zeroToHeroOne/exercise24.java
@@ -0,0 +1,43 @@
+package com.zeroToHeroOne;
+
+import java.util.Scanner;
+
+public class exercise24 {
+    //Faça um algoritmo que calcule a quantidade de litros de combustível gastos em uma viagem,
+    // sabendo que o carro faz 12km com um litro.
+    // Deve-se fornecer ao usuário o tempo que será gasto na viagem a sua velocidade média,
+    // distância percorrida e a quantidade de litros utilizados para fazer a viagem.
+    //Fórmula: distância = tempo x velocidade.
+    // litros usados = distância / 12.
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        float tempo, velocidade, distancia, litrosUtilizados;
+        int menu;
+        boolean novaConsulta = true;
+
+            while(novaConsulta){
+            System.out.println("Digite quanto tempo durou a viagem? ");
+            tempo = scan.nextFloat();
+
+            System.out.println("Digite a velocidade média? ");
+            velocidade = scan.nextFloat();
+
+            distancia = tempo * velocidade;
+            litrosUtilizados = distancia / 12;
+            System.out.printf("Você percorreu: %.2f km de distância e a gastou %.2f litros de combustível.", distancia, litrosUtilizados);
+            System.out.println("");
+
+            System.out.println("Deseja fazer uma nova consulta?");
+            System.out.println("1 - Sim");
+            System.out.println("2 - Não");
+            menu = scan.nextInt();
+
+            if(menu == 2){
+                novaConsulta = false;
+
+            }
+        }
+        scan.close();
+    }
+}
